6808ec2f5abe5d92ce8723ddc0451a3ebfc08565
shahd_Elmnyar <shahdmohamed2632003@gmail.com>
2024-07-20 14:56:42 +0300

    .

--- start js task/4/script.js
+++ start js task/4/script.js
@@ -6,8 +6,7 @@ function checkPositiveNegative(num1, num2) {
     }
 }
 
-// Changed to prompt for user input
-const num1 = parseFloat(prompt("Enter the first number:")); 
-const num2 = parseFloat(prompt("Enter the second number:")); 
+const num1 = number(prompt("Enter the first number:")); 
+const num2 = number(prompt("Enter the second number:")); 
 
 console.log(checkPositiveNegative(num1, num2)); 
