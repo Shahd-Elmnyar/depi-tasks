2357344466e026127f1d94a60b7e4e196a8474f3
shahd_Elmnyar <shahdmohamed2632003@gmail.com>
2024-07-22 19:22:43 +0300

    .

--- background_generator/script.js
+++ background_generator/script.js
@@ -1,15 +1,12 @@
-var css = document.querySelector("h3");
-        var color1 = document.querySelector(".color1");
-        var color2 = document.querySelector(".color2");
-        var body = document.querySelector(".change");
-        function changeGradient() {
-            body.style.background =
-                "linear-gradient(to right, "
-                    + color1.value + ", "
-                    + color2.value + ")";
-        
-        }
-        
-        color1.addEventListener("input", changeGradient);
-        
-        color2.addEventListener("input", changeGradient);
+
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.querySelector(".change");
+
+const changeGradient = () => {
+    body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+    css.textContent = `${body.style.background};`;
+};
+
+color1.addEventListener("input", changeGradient);
+color2.addEventListener("input", changeGradient);
